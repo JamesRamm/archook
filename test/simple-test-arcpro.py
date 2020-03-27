9496325d749d46570ea18958e53eede68d36de2a
Matt Wilkie <matt.wilkie@gov.yk.ca>
2020-03-27 12:11:53 -0700

    debugging #22
    ...but mostly testing to see if my github oauth token is sorted out.

--- test/simple-test-arcpro.py
+++ test/simple-test-arcpro.py
@@ -1,7 +1,25 @@
 # manual test (not a unit test)
+print("="*40)
+import sys
 import archook
+
+pre = []
+for p in sys.path:
+    pre.append(p)
 
 r = archook.get_arcpy(pro=True)
 print("ArcGIS Pro archook result: %s" % r)
-import arcpy
-print(f"Arcpy Workspace: {arcpy.env.workspace}")
+
+post = []
+for p in sys.path:
+    post.append(p)
+
+try:
+    import arcpy
+    print(f"Arcpy Workspace: {arcpy.env.workspace}")
+
+except ImportError as e:
+    print(f"\n{e}\n")
+    print(f"\nPre sys.path: {pre}")
+    print(f"\nPost sys.path: {post}")
+
