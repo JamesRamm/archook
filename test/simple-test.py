77372b236de5656c51edb6f37e8b940364002296
Matt Wilkie <matt.wilkie@gov.yk.ca>
2020-03-27 11:25:15 -0700

    Delete simple-test.py

--- test/simple-test.py
+++
@@ -1,8 +0,0 @@
-# manual test (not a unit test)
-import archook
-
-r = archook.get_arcpy()
-print("ArcGIS Desktop archook result: %s" % r)
-
-r = archook.get_arcpy(pro=True)
-print("ArcGIS Pro archook result: %s" % r)
