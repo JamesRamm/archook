e6356a631532b1b90cf22ddca3b552324b880557
mhw-at-yg <matt.wilkie@gov.yk.ca>
2020-04-02 10:38:31 -0700

    recipe to create conda env that works with ArcMap
    To test that @logan-pugh in #22 is correct, bitness matters
    (Answer is: yes)

---
+++ test/create-conda-py27-32-env.bat
@@ -0,0 +1,10 @@
+set CONDA_FORCE_32BIT=1
+conda create -n archook-py27-32 python=2.7
+@echo.
+@echo. Next steps: 
+@echo.
+@echo.   conda activate archook-py27-32
+@echo.   conda install numpy
+@echo. 
+@echo.   Test archook
+@echo.
