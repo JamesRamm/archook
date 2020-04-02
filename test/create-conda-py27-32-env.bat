5d3e84ed365af7f9e00acee3c2c58b35fec90316
mhw-at-yg <matt.wilkie@gov.yk.ca>
2020-04-02 13:47:27 -0700

    bugfix: use call so control returns to script

--- test/create-conda-py27-32-env.bat
+++ test/create-conda-py27-32-env.bat
@@ -1,10 +1,9 @@
 set CONDA_FORCE_32BIT=1
-conda create -n archook-py27-32 python=2.7
-@echo.
-@echo. Next steps: 
-@echo.
-@echo.   conda activate archook-py27-32
-@echo.   conda install numpy
+call conda create -n archook-py27-32 python=2.7
+call conda activate archook-py27-32
+call conda install numpy
+@echo. --------------------------------
+@echo. Next step: 
 @echo. 
 @echo.   Test archook
 @echo.
