0ee4fb62d824ffb159b83423a1681a89ca289151
mhw-at-yg <matt.wilkie@gov.yk.ca>
2020-05-12 10:45:27 -0700

    run through black formatting tool

--- archook/archook.py
+++ archook/archook.py
@@ -1,8 +1,8 @@
-'''
+"""
 Locate ArcPy and add it to the path
 Created on 13 Feb 2015
 @author: Jamesramm
-'''
+"""
 try:
     import _winreg
 except ImportError:
@@ -12,53 +12,62 @@ import sys
 import struct
 import inspect
 
+
 def get_python_bitness():
-    '''Return bit size of active python interpreter (ie. 32, 64)'''
-    return struct.calcsize('P')*8
+    """Return bit size of active python interpreter (ie. 32, 64)"""
+    return struct.calcsize("P") * 8
+
 
 def get_arc_bitness(pro=False):
-    '''Return 32 or 64 bit nature of ArGIS binaries.'''
+    """Return 32 or 64 bit nature of ArGIS binaries."""
     # Pro is always 64bit
     if pro:
         return 64
     install_dir = locate_arcgis()
     # print(install_dir) # debug
-    if os.path.exists(os.path.join(install_dir, 'bin64')):
+    if os.path.exists(os.path.join(install_dir, "bin64")):
         return 64
-    elif os.path.exists(os.path.join(install_dir, 'bin')):
+    elif os.path.exists(os.path.join(install_dir, "bin")):
         return 32
     return None
 
+
 def verify_bit_match(pro=False):
-    '''Return true if python interpreter and ArcGIS bitness match each other'''
+    """Return true if python interpreter and ArcGIS bitness match each other"""
     pybits = get_python_bitness()
     arcbits = get_arc_bitness(pro)
     match = pybits == arcbits
     if not match:
-        msg = "*** Error: python and arcgis 32/64bit mismatch: Py:{}, Arc:{}".format(pybits, arcbits)
+        msg = "*** Error: python and arcgis 32/64bit mismatch: Py:{}, Arc:{}".format(
+            pybits, arcbits
+        )
         raise Exception(msg)
     return match
 
+
 def verify_conda_meta_dir():
-    '''Issue warning if conda-meta folder does not exist
+    """Issue warning if conda-meta folder does not exist
 
     Arcpy checks folder exists regardless of whether actually using conda.
     (https://github.com/JamesRamm/archook/issues/22#issuecomment-624262435)
-    '''
+    """
     cmeta = os.path.join(sys.exec_prefix, "conda-meta")
     if not os.path.exists(cmeta):
-        print(f"""
+        print(
+            f"""
 Doesn't exist:
    {cmeta}
 
    You may need to create this directory if you get an error like:
 
    ImportError("arcpy needs to run within an active ArcGIS Conda environment")
-""")
+"""
+        )
     return
 
+
 def locate_arcgis(pro=False):
-    '''
+    """
     Find the path to the ArcGIS Desktop installation, or the ArcGIS Pro installation
     if `pro` argument is True.
 
@@ -71,39 +80,35 @@ def locate_arcgis(pro=False):
     HKLM/SOFTWARE/ESRI/DesktopXX.X 'InstallDir'. Where XX.X is the version
 
     We may need to check HKLM/SOFTWARE/Wow6432Node/ESRI instead
-    '''
+    """
     try:
         if pro:
             pro_key = get_pro_key()
-            install_dir = _winreg.QueryValueEx(pro_key, 'InstallDir')[0]
+            install_dir = _winreg.QueryValueEx(pro_key, "InstallDir")[0]
         else:
             key = _winreg.OpenKey(
-                _winreg.HKEY_LOCAL_MACHINE,
-                r'SOFTWARE\Wow6432Node\ESRI\ArcGIS',
-                0
+                _winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\Wow6432Node\ESRI\ArcGIS", 0
             )
 
-            version = _winreg.QueryValueEx(key, 'RealVersion')[0][:4]
+            version = _winreg.QueryValueEx(key, "RealVersion")[0][:4]
 
-            key_string = r'SOFTWARE\Wow6432Node\ESRI\Desktop{0}'.format(version)
-            desktop_key = _winreg.OpenKey(
-                _winreg.HKEY_LOCAL_MACHINE,
-                key_string,
-                0
-            )
+            key_string = r"SOFTWARE\Wow6432Node\ESRI\Desktop{0}".format(version)
+            desktop_key = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, key_string, 0)
 
-            install_dir = _winreg.QueryValueEx(desktop_key, 'InstallDir')[0]
+            install_dir = _winreg.QueryValueEx(desktop_key, "InstallDir")[0]
         return install_dir
     except WindowsError:
-        raise ImportError('Could not locate the ArcGIS directory on this machine')
+        raise ImportError("Could not locate the ArcGIS directory on this machine")
+
 
 def get_pro_paths():
-    '''Return 2 lists, for adding to Windows PATH and python sys.path'''
+    """Return 2 lists, for adding to Windows PATH and python sys.path"""
     P = locate_arcgis(pro=True)
     C = locate_pro_conda()
     # P = r"C:\Program Files\ArcGIS\Pro"
     # C = r"C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3"
-    PRO_WIN_PATHS = inspect.cleandoc(rf"""
+    PRO_WIN_PATHS = inspect.cleandoc(
+        rf"""
         {C}
         {C}\Library\mingw-w64
         {C}\Library\usr\bin
@@ -111,8 +116,10 @@ def get_pro_paths():
         {C}\Scripts
         {P}\Python\Scripts
         {P}\bin
-        """)
-    PRO_SYSPATHS = inspect.cleandoc(rf"""
+        """
+    )
+    PRO_SYSPATHS = inspect.cleandoc(
+        rf"""
         {C}
         {C}\python36.zip
         {C}\DLLs
@@ -121,18 +128,20 @@ def get_pro_paths():
         {P}\bin
         {P}\Resources\ArcPy
         {P}\Resources\ArcToolbox\Scripts
-        """)
+        """
+    )
     winpaths = PRO_WIN_PATHS.splitlines()
     syspaths = PRO_SYSPATHS.splitlines()
     return [winpaths, syspaths]
 
+
 def get_arcpy(pro=False):
-    '''
+    """
     Allows arcpy to imported on 'unmanaged' python installations (i.e. python installations
     arcgis is not aware of).
     Gets the location of arcpy and related libs and adds it to sys.path
     Looks for ArcGIS Pro if `pro` argument is True.
-    '''
+    """
     install_dir = locate_arcgis(pro)
 
     if pro:
@@ -142,26 +151,28 @@ def get_arcpy(pro=False):
 
         winpaths, syspaths = get_pro_paths()
         # update Windows PATH
-        wp = os.environ['PATH'].split(';') # save incoming path
-        [wp.insert(0,x) for x in winpaths] # prepend our new syspath
-        os.environ['PATH'] = ';'.join(wp) # write back to environment
+        wp = os.environ["PATH"].split(";")  # save incoming path
+        [wp.insert(0, x) for x in winpaths]  # prepend our new syspath
+        os.environ["PATH"] = ";".join(wp)  # write back to environment
         # update sys.path
         [sys.path.insert(0, x) for x in syspaths]
 
     else:
         verify_bit_match()
-        arcpy = os.path.join(install_dir, 'arcpy')
+        arcpy = os.path.join(install_dir, "arcpy")
         # Check we have the arcpy directory.
         if not os.path.exists(arcpy):
-            raise ImportError('Could not find arcpy directory in {0}'.format(install_dir))
+            raise ImportError(
+                "Could not find arcpy directory in {0}".format(install_dir)
+            )
 
         if get_arc_bitness() == 64:
-            bin_dir = os.path.join(install_dir, 'bin64')
+            bin_dir = os.path.join(install_dir, "bin64")
         else:
-            bin_dir = os.path.join(install_dir, 'bin')
+            bin_dir = os.path.join(install_dir, "bin")
 
         # Update Python's path
-        dirs = ['', arcpy, bin_dir, 'ArcToolbox/Scripts']
+        dirs = ["", arcpy, bin_dir, "ArcToolbox/Scripts"]
         for p in dirs:
             sys.path.insert(0, os.path.join(install_dir, p))
 
@@ -179,40 +190,45 @@ def get_arcpy(pro=False):
         # sys.path.extend([arcpy, bin_dir, scripts])
 
 
-
 def locate_pro_conda():
-    '''
+    """
     Returns the path to the ArcGIS Pro-managed conda environment.
-    '''
+    """
     try:
         pro_key = get_pro_key()
-        conda_root = _winreg.QueryValueEx(pro_key, 'PythonCondaRoot')[0]
-        conda_env = _winreg.QueryValueEx(pro_key, 'PythonCondaEnv')[0]
-        conda_path = os.path.join(conda_root, 'envs', conda_env)
+        conda_root = _winreg.QueryValueEx(pro_key, "PythonCondaRoot")[0]
+        conda_env = _winreg.QueryValueEx(pro_key, "PythonCondaEnv")[0]
+        conda_path = os.path.join(conda_root, "envs", conda_env)
         if not os.path.exists(conda_path):
-            raise ImportError('Could not find Conda environment {} in root directory {}'.format(conda_env, conda_root))
+            raise ImportError(
+                "Could not find Conda environment {} in root directory {}".format(
+                    conda_env, conda_root
+                )
+            )
         return conda_path
     except WindowsError:
-        raise ImportError('Could not locate the Conda directory on this machine')
+        raise ImportError("Could not locate the Conda directory on this machine")
 
 
 def get_pro_key():
-    '''
+    """
     Returns ArcGIS Pro's registry key.
-    '''
-    pro_key = _winreg.OpenKey(
-        _winreg.HKEY_LOCAL_MACHINE,
-        r'SOFTWARE\ESRI\ArcGISPro'
-    )
+    """
+    pro_key = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\ESRI\ArcGISPro")
     return pro_key
+
 
 def thankyou():
     d = {}
-    d['@logan-pugh'] = 'Python and Arc bit level must be the same\n\t (https://github.com/JamesRamm/archook/issues/22)'
-    d['@ChristopheD'] = 'Concise bitness for any python version\n\t (https://stackoverflow.com/questions/1405913/how-do-i-determine-if-my-python-shell-is-executing-in-32bit-or-64bit-mode-on-os)'
+    d[
+        "@logan-pugh"
+    ] = "Python and Arc bit level must be the same\n\t (https://github.com/JamesRamm/archook/issues/22)"
+    d[
+        "@ChristopheD"
+    ] = "Concise bitness for any python version\n\t (https://stackoverflow.com/questions/1405913/how-do-i-determine-if-my-python-shell-is-executing-in-32bit-or-64bit-mode-on-os)"
 
-    print('='*72)
+    print("=" * 72)
     print("Thank you for the help:\n")
     for k, v in d.items():
         print("{}:\n\t{}".format(k, v))
-    print('='*72)
+    print("=" * 72)
--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='archook',
+    name="archook",
     version="1.2.0",
-    description='Locates arcpy and makes it available to the running python distribution',
-    maintainer='James Ramm',
-    maintainer_email='jamessramm@gmail.com',
-    url='https://github.com/JamesRamm/archook',
+    description="Locates arcpy and makes it available to the running python distribution",
+    maintainer="James Ramm",
+    maintainer_email="jamessramm@gmail.com",
+    url="https://github.com/JamesRamm/archook",
     packages=find_packages(),
     include_package_data=True,
     classifiers=[
-        'Environment :: Console',
-        'Intended Audience :: Science/Research',
-        'Programming Language :: Python',
-        'Topic :: Scientific/Engineering',
-        'Operating System :: Microsoft :: Windows'
-    ]
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering",
+        "Operating System :: Microsoft :: Windows",
+    ],
 )
