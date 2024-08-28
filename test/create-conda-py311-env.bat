5f35971b298079ea048a432ab188dffcadd0eca4
matt wilkie <mapew@gmail.com>
2024-08-28 13:03:52 -0700

    py311 scripts
    One day we'll merge these into a single creator and setter script.
    Today is not that day though. ; -)

---
+++ test/create-conda-py311-env.bat
@@ -0,0 +1,12 @@
+call conda create -y --name archook-py311 python=3.11 -y
+call conda activate  archook-py311
+@REM call conda install -y numpy
+@echo. --------------------------------
+@echo. Next steps:
+@echo. 
+@echo.   python -m pip install --editable ..
+@echo. or
+@echo.   python -m pip install ..
+@echo.
+@echo.   Test archook
+@echo.
